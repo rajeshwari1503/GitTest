5302f5fff039bfa87c829239fbd2748825d7ae15
Divya <raji.rajeshwari970@gmail.com>
2022-11-29 16:11:58 +0530

    Raji Message

--- src/MyPackage/LaunchBrowsers.java
+++ src/MyPackage/LaunchBrowsers.java
@@ -1,16 +1,18 @@
 package MyPackage;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 
 public class LaunchBrowsers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.setProperty("webdriver.chrome.driver","Chromedriver");
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\home\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	driver.get("www.amazon.com");
+	String Title = driver.getTitle();
+	System.out.println(Title);
 	
 	
 	
